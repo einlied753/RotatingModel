23620e9c9bdcce0f50d63e0753a59e5320d88535
einlied753 <einlied753@gmail.com>
2022-10-05 20:14:01 +0300

    initial commit

---
+++ BodyRotation/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace BodyRotation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RenderTimer = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(500, 500);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GL_POINTS",
+            "GL_LINES",
+            "GL_QUADS"});
+            this.comboBox1.Location = new System.Drawing.Point(525, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // RenderTimer
+            // 
+            this.RenderTimer.Interval = 30;
+            this.RenderTimer.Tick += new System.EventHandler(this.RenderTimer_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(601, 50);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 288);
+            this.trackBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 555);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.Text = "BodyRotation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer RenderTimer;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,32 @@
+# Body rotation
+
+The Win Form project represents a 3D model of electromagnetic wave diffraction on a sphere. 
+The method of numerical solution of the system of Maxwell's equations was used in the work to calculate the diffraction of a plane wave on a sphere.
+
+### Used technologies
+
+OpenGL 2.1.0.12 and Tao Framework library
+
+### The view of a 3D diffraction model
+
+(docs/3d model_1.png)
+(docs/3d model_2.png)
+(docs/3d model_3.png)
+
+### The solution
+
+The components of the electric intensity vector:
+
+(docs/Components of the electric intensity vector.png)
+
+The coefficients of the series of the field diffracted from the sphere:
+
+(docs/Coefficients of the series of the field diffracted from the sphere.png)
+
+Other coefficients:
+
+(docs/Other coefficients.png)
+
+The components of the vector E:
+
+(docs/Components of the vector E.png)
